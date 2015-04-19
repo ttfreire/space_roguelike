dac2a85d5175e485ab3fb88100effb6f7617b5a7
Matheus Freire <matheusfreire@gmail.com>
2015-04-19 15:51:08 -0300

    Room loading changes

--- Assets/loadRoom.cs
+++ Assets/loadRoom.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 
 public class loadRoom : MonoBehaviour {
-	public int m_room;
+	public int[] m_rooms;
+	[HideInInspector]public int m_room;
 	public bool isLoaded;
 	// Use this for initialization
 	void Awake () {
 		isLoaded = false;
+		m_room = m_rooms[Random.Range(0, m_rooms.Length)];
 	}
 	
 	// Update is called once per frame
