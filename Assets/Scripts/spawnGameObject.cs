a5afc6dacf8dea7a10a5d4c312095321a19497fe
Matheus Freire <matheusfreire@gmail.com>
2015-05-01 10:35:44 -0300

    Dynamic Spawner
    WE DON’T NEED YOU, PREFAB INSIDE PREFAB
    
    (only changed enemies at rooms… please follow the pattern for itens at
    rooms and everything on chunks)

---
+++ Assets/Scripts/spawnGameObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnGameObject : MonoBehaviour {
+	public GameObject objectToSpawn;
+	// Use this for initialization
+	void Start () {
+		GameObject spawned = Instantiate (objectToSpawn, this.transform.position, this.transform.rotation) as GameObject;
+		spawned.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
