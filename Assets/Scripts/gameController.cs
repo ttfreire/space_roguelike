28bce1a073d8e6bd55fc34d30041d92ea5a3b85b
Matheus Freire <matheusfreire@gmail.com>
2015-05-09 01:18:16 -0300

    Removed obsolete information from interface

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -7,10 +7,7 @@ public class gameController : MonoBehaviour {
 
 	public static gameController control;
 	public bool generateLevel;
-	public bool isUsingJoystick = false;
-	public Text theInput;
 	public Text scrap1;
-	public Text scrap2;
 	public Text victory;
 	public Image m_currentOxygen;
 
@@ -41,24 +38,22 @@ public class gameController : MonoBehaviour {
 		if (Input.GetKeyUp (KeyCode.U))
 			player.GetComponent<upgradeController> ().UpgradetoLevel (m_pControl.m_level);
 
-		if (Input.GetKeyUp (KeyCode.I)) {
+		if (Input.GetKeyUp (KeyCode.I))
 			m_pControl.scrap1Quantity += 5;
-			m_pControl.scrap2Quantity += 5;
-		}
+
 		if (Input.GetKeyUp (KeyCode.R))
 			Application.LoadLevel (Application.loadedLevel);
-		if (Input.GetKeyUp (KeyCode.J))
-			isUsingJoystick = !isUsingJoystick;
-		if (Input.GetKeyUp (KeyCode.T)){
+
+		if (Input.GetKeyUp (KeyCode.T))
 			KillAllEnemies();
-		}
+
 		if (Input.GetKeyUp (KeyCode.F))
 			HideEnemiesFOV();
 
-			if (Input.GetKeyUp (KeyCode.L)){
-				GameObject sceneRoot = GameObject.Find("_ROOT");
-				Destroy(sceneRoot);
-			}
+		if (Input.GetKeyUp (KeyCode.L)){
+			GameObject sceneRoot = GameObject.Find("_ROOT");
+			Destroy(sceneRoot);
+		}
 		InterfaceFeedback ();
 
 	}
@@ -66,7 +61,6 @@ public class gameController : MonoBehaviour {
 	void InterfaceFeedback(){
 		m_currentOxygen.fillAmount = m_pHealth.m_currentOxygenValue/m_pHealth.m_playerTotalOxygen;
 		scrap1.text = m_pControl.scrap1Quantity.ToString ();
-		scrap2.text = m_pControl.scrap2Quantity.ToString ();
 
 		if (m_pControl.endlevel) {
 			victory.text = "Victory!";
