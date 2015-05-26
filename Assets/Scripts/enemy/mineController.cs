3b8bbc4d2e41d985496de0f7129bca5cf0884617
Matheus Freire <matheusfreire@gmail.com>
2015-05-26 13:59:16 -0300

    First mine
    Yes, I'm using Droid sprites and animation

---
+++ Assets/Scripts/enemy/mineController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mineController : enemyBaseController {
+	public float m_pushForce;
+	// Use this for initialization
+	void Awake () {
+		base.Awake ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update ();
+	}
+
+	void pullTarget(Rigidbody target){
+		Vector3 pullVector = (transform.position - target.transform.position) * m_pushForce;
+		target.AddForceAtPosition (pullVector, target.transform.position, ForceMode.Impulse);
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.tag.Equals ("Player"))
+			pullTarget (other.gameObject.rigidbody);
+	}
+}
