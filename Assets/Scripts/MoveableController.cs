78117a575f935e8732c6e8800348949637f25c1e
Matheus Freire <matheusfreire@gmail.com>
2015-06-11 20:36:41 -0300

    Some changes
    Confia em mim, parca

--- Assets/Scripts/MoveableController.cs
+++ Assets/Scripts/MoveableController.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 
 public class MoveableController : MonoBehaviour {
-
+	float m_force;
 	// Use this for initialization
 	void Start () {
+		m_force = 10;
 	
 	}
-	
 	// Update is called once per frame
 	void Update () {
 
 	}
 
 	void OnCollisionEnter(Collision other){
-		if(other.gameObject.tag.Equals("Projectile"))
+		if(other.gameObject.tag.Equals("Projectile")){
+			Vector3 vectorForce = (other.gameObject.transform.position - this.transform.position )* m_force;
+			this.gameObject.rigidbody.AddForceAtPosition(vectorForce, this.gameObject.transform.position);
 			Destroy(other.gameObject);
+		}
 	}
 }
