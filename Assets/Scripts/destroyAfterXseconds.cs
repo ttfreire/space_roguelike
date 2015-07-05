83d32b2741b826dd0186a20566c890cb81a1dca4
Matheus Freire <matheusfreire@gmail.com>
2015-07-05 18:53:26 -0300

    Added some features to the explosion prefab
    Added a script to destroy the explosions after 1 second
    Added particles as explosion object child

---
+++ Assets/Scripts/destroyAfterXseconds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyAfterXseconds : MonoBehaviour {
+	public float SecondsToDestroy;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (SecondsToDestroy <= 0.0f)
+			Destroy (gameObject);
+		SecondsToDestroy -= Time.deltaTime;
+	}
+}
