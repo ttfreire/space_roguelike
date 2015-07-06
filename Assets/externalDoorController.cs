7ad96f2e0868ff032b0e14bb66cc66e0f686f0f7
Matheus Freire <matheusfreire@gmail.com>
2015-07-05 21:39:18 -0300

    External door prefab created

---
+++ Assets/externalDoorController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class externalDoorController : MonoBehaviour {
+	Animator anim;
+	bool isClosed = true;
+	public Sprite closedSprite;
+	public Sprite openedSprite;
+	SpriteRenderer doorSprite;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		doorSprite = GetComponent<SpriteRenderer> ();
+		doorSprite.sprite = closedSprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isClosed)
+			doorSprite.sprite = closedSprite;
+		else
+			doorSprite.sprite = openedSprite;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag.Equals ("Player")) {
+			anim.SetBool ("closeToDoor", true);
+			isClosed = false;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag.Equals ("Player")) {
+			anim.SetBool ("closeToDoor", false);
+			isClosed = true;
+		}
+	}
+}
