7b6557c3768f95dd8dab56205501bda3ba4c3700
Matheus Freire <matheusfreire@gmail.com>
2015-07-08 20:35:00 -0300

    Fixed bug where player give up from game and main screen freezes

--- Assets/presentationController.cs
+++ Assets/presentationController.cs
@@ -2,18 +2,27 @@
 using System.Collections;
 
 public class presentationController : MonoBehaviour {
-	float timer = 2;
+	float timer;
 	public GameObject Menu;
+	public Canvas start;
+	public Canvas idle;
 	// Use this for initialization
 	void Start () {
-	
+		timer = 2;
+		Time.timeScale = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (timer > 0)
+		if (timer > 0) {
 			timer -= Time.deltaTime;
-		if (timer < 0)
-			Menu.SetActive(true);
+			Debug.Log("na");
+		}
+		if (timer < 0) {
+			Menu.SetActive (true);
+			start.gameObject.SetActive(false);
+			idle.gameObject.SetActive(true);
+			timer = 0;
+		}
 	}
 }
