41c803f17f031e86d6aa744903a2e88e66d1dd21
Matheus Freire <matheusfreire@gmail.com>
2015-07-09 12:44:38 -0300

    Intro scene

---
+++ Assets/introcontroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class introcontroller : MonoBehaviour {
+	float timer;
+	// Use this for initialization
+	void Start () {
+		timer = 6;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > 0)
+			timer -= Time.deltaTime;
+		else
+			Application.LoadLevel ("MainScreen");
+	}
+}
